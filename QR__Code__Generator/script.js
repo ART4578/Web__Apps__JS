c774a4ae54f67010e998571b524b4a24d8b97359
ART4578 <artur17-00@inbox.ru>
2024-04-30 12:03:38 +0400

    Web__Apps__JS

---
+++ QR__Code__Generator/script.js
@@ -0,0 +1,42 @@
+function qr__code() {
+    const wrapper = document.querySelector(".wrapper");
+    const qrInput = wrapper.querySelector(".form input");
+    const generateBtn = wrapper.querySelector("#generate");
+    const qrImg = wrapper.querySelector("#qr-image");
+    const downloadBtn = wrapper.querySelector("#download-btn");
+    const downloadLink = wrapper.querySelector("#download-link");
+
+    let preValue;
+
+    generateBtn.addEventListener("click", () => {
+        let qrValue = qrInput.value.trim();
+
+        if (!qrValue || preValue === qrValue) return;
+
+        preValue = qrValue;
+
+        generateBtn.innerText = "Generating QR Code...";
+
+        qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+        qrImg.addEventListener("load", () => {
+            wrapper.classList.add("active");
+            generateBtn.innerText = "Generate QR Code";
+            downloadBtn.disabled = false;
+            downloadLink.href = qrImg.src;
+        });
+    });
+
+    downloadBtn.addEventListener("click", () => {
+        downloadBtn.click();
+    });
+
+    qrInput.addEventListener("keyup", () => {
+        if (!qrInput.value.trim()) {
+            wrapper.classList.remove("active");
+            preValue = "";
+            downloadBtn.disabled = true;
+        };
+    });
+};
+
+qr__code();
