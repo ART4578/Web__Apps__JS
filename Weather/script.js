98f6de76b9dc28862535fc725755bb5b96bc8826
ART4578 <artur17-00@inbox.ru>
2025-05-23 18:31:02 +0400

    Web__Apps__JS

---
+++ Weather/script.js
@@ -0,0 +1,66 @@
+class Weather {
+    constructor() {
+        this.cityInput = document.querySelector(".search input");
+        this.button = document.querySelector(".search button");
+        this.weatherIcon = document.querySelector(".weather-icon");
+        this.weather = document.querySelector(".weather");
+        this.error = document.querySelector(".error");
+        this.cityName = document.querySelector(".city");
+        this.temp = document.querySelector(".temp");
+        this.humidity = document.querySelector(".humidity");
+        this.wind = document.querySelector(".wind");
+
+        this.apiKey = "7b11065fd7317444e4beedf5d19ecba8";
+
+        this.button.addEventListener("click", () => this.checkWeather());
+    };
+
+    async checkWeather() {
+        const cityValue = this.cityInput.value.trim();
+
+        if (!cityValue) {
+            alert("Մուտքագրեք քաղաքի անունը։");
+        } else {
+            const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${this.apiKey}&units=metric`;
+
+            const response = await fetch(apiUrl);
+
+            if (response.status == 404) {
+                this.error.style.display = "block";
+                this.weather.style.display = "none";
+            } else {
+                const data = await response.json();
+
+                this.cityName.innerHTML = data.name;
+                this.temp.innerHTML = Math.round(data.main.temp) + "°c";
+                this.humidity.innerHTML = data.main.humidity + "%";         
+                this.wind.innerHTML = data.wind.speed + " km/h";
+
+                const mainWeather = data.weather[0].main;
+
+                switch (mainWeather) {
+                    case "Clouds":
+                        this.weatherIcon.src = "./img/clouds.png";
+                        break;
+                    case "Clear":
+                        this.weatherIcon.src = "./img/clear.png";
+                        break;
+                    case "Rain":
+                        this.weatherIcon.src = "./img/rain.png";
+                        break;
+                    case "Drizzle":
+                        this.weatherIcon.src = "./img/drizzle.png";
+                        break;
+                    case "Mist":
+                        this.weatherIcon.src = "./img/mist.png";
+                        break;                
+                };
+
+                this.weather.style.display = "block";
+                this.error.style.display = "none";
+            };
+        };
+    };
+};
+
+new Weather();
