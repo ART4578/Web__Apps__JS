b66ffd3367c17e54885c6e4b6fb2b62bd96f4496
ART4578 <artur17-00@inbox.ru>
2026-01-26 12:52:05 +0400

    Web__Apps__JS

--- QR__Code__Generator/script.js
+++ QR__Code__Generator/script.js
@@ -17,16 +17,17 @@ generateBtn.addEventListener("click", () => {
     generateBtn.innerText = "Generating QR Code...";
 
     qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
-        qrImg.addEventListener("load", () => {
-            wrapper.classList.add("active");
-            generateBtn.innerText = "Generate QR Code";
-            downloadBtn.disabled = false;
-            downloadLink.href = qrImg.src;
-        });
+
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+        downloadBtn.disabled = false;
+        downloadLink.href = qrImg.src;
+    });
 });
 
 downloadBtn.addEventListener("click", () => {
-        downloadBtn.click();
+    downloadBtn.click();
 });
 
 qrInput.addEventListener("keyup", () => {
